9da695eb5a94aedfefb4cb77b78513a9eec04389
yuri464946 <takasakiyuri555@gmail.com>
2023-09-12 21:15:46 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'prototypes/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root to: "prototypes#index"
+end
