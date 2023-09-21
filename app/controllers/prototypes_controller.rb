de4408ca0cbed9c89514d9859435dc7919a607ca
yuri464946 <takasakiyuri555@gmail.com>
2023-09-21 17:20:40 +0900

    投稿機能を実装のためcreateアクションを作成

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -5,7 +5,36 @@ class PrototypesController < ApplicationController
   def new
     @prototype=Prototype.new
   end
-  def prototypes_params
-    params.require(:prototypes).permit(:content, :image, :title,:catch_copy).merge(user_id: current_user.id)
+
+  def create
+    @prototype=Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    prototype.update(prototype_params)
+    redirect_to root_path
+  end
+
+
+  private
+  
+  def prototype_params
+    params.require(:prototype).permit(:content, :image, :title,:catch_copy,:concept).merge(user_id: current_user.id)
+  end
+
+
+
+
+
+
+
+
+
+  
 end
