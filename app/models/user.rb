809b4f9bc7e899754f33867a6beebc47e667558b
yuri464946 <takasakiyuri555@gmail.com>
2023-09-24 17:49:06 +0900

    コメント投稿機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name,presence: true
+  validates :position,presence: true
+  validates :occupation,presence: true
+  validates :position,presence: true
+
   has_many :prototypes 
-     
+  has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "prototypes#index"
-  resources :prototypes
-
+  resources :prototypes do
+    resources :comments, only: :create
+  end
 end
