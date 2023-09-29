e2a07276a8a2c1ecabc8034e33b22ac1991161cb
yuri464946 <takasakiyuri555@gmail.com>
2023-09-29 17:26:35 +0900

    削除機能修正

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -5,6 +5,6 @@ class Prototype < ApplicationRecord
   validates :image, presence: true
 
   belongs_to       :user
-  has_many         :comments
+  has_many         :comments,dependent: :destroy
   has_one_attached :image
 end
